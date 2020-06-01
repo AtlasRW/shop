37439bedf4217d68cf97f99b8161720033f27bab
TonyStark992 <maxime.castillo@gmail.com>
2020-06-01 20:19:10 +0200

    Seed fait, en attente de test sur BDD réelle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Item.destroy_all
+
+puts "Seeding database"
+
+# Génération des items
+20.times do
+  Item.create!(
+    title: Faker::Creature::Cat.name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    price: rand(1..1000),
+    image_url: Faker::Creature::Cat.image(grayscale: true))
+end
+puts "-> some items have been generated "
+
+puts "Done !"
