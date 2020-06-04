1184195630da83f1e61a78861e4233c2f40f987f
Redfact <tantelyrandria@ymail.com>
2020-06-04 23:50:58 +0200

    generate admin interface(no admin auth)  and migrate user table

--- db/migrate/20200604151706_add_columns_to_users.rb
+++ db/migrate/20200604151706_add_columns_to_users.rb
@@ -5,6 +5,8 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.2]
       t.column :last_name, :string
       t.column :age, :integer
       t.column :description, :text
+      t.column :admin , :boolean , :default => false
+
     end
   end
 end
