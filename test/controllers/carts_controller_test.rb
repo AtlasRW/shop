6bd23976ab52e3f2d86c7a7707cd9cc7f601a655
AtlasRW <rw_protonmail.com>
2020-06-04 11:09:23 +0200

    Revert "cart"
    This reverts commit cf0f3dabb6188b7c4a4a451a49a1e74d8e0b2535.

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,48 +1,34 @@
 require 'test_helper'
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @cart = carts(:one)
-  end
-
-  test "should get index" do
-    get carts_url
+  test "should get show" do
+    get carts_show_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_cart_url
+    get carts_new_url
     assert_response :success
   end
 
-  test "should create cart" do
-    assert_difference('Cart.count') do
-      post carts_url, params: { cart: {  } }
-    end
-
-    assert_redirected_to cart_url(Cart.last)
-  end
-
-  test "should show cart" do
-    get cart_url(@cart)
+  test "should get create" do
+    get carts_create_url
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_cart_url(@cart)
+    get carts_edit_url
     assert_response :success
   end
 
-  test "should update cart" do
-    patch cart_url(@cart), params: { cart: {  } }
-    assert_redirected_to cart_url(@cart)
+  test "should get update" do
+    get carts_update_url
+    assert_response :success
   end
 
-  test "should destroy cart" do
-    assert_difference('Cart.count', -1) do
-      delete cart_url(@cart)
-    end
-
-    assert_redirected_to carts_url
+  test "should get delete" do
+    get carts_delete_url
+    assert_response :success
   end
+
 end
