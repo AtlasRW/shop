0edd1d128207a982a2728f85a6eb9fcdcfb7a7b0
gringer-and-granger <62955280+gringer-and-granger@users.noreply.github.com>
2020-06-04 17:03:53 +0200

    stripe (#12)

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,25 @@
+class ChargesController < ApplicationController
+	def new
+end
+
+def create
+  # Amount in cents
+  @amount = @cart.total_price * 100
+
+  customer = Stripe::Customer.create({
+    email: params[:stripeEmail],
+    source: params[:stripeToken],
+  })
+
+  charge = Stripe::Charge.create({
+    customer: customer.id,
+    amount: @amount,
+    description: 'Paiement de #{current_user.first_name} #{current_user.last_name}',
+    currency: 'eur',
+  })
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to new_charge_path
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items, only: [:index, :show]
   devise_for :users
+  resources :charges
 end
