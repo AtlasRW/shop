dd58922c34d9a09b29c79cc3c399e57e167ec003
atlasrw <atlasrw@icloud.com>
2021-04-25 02:27:16 +0200

    clean

--- db/migrate/20200603103311_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.references :user , index: true
-      t.references :item , index: true
-      t.timestamps
-    end
-  end
-end
