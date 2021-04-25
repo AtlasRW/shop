ab5ca4cbc1a50c60fe7baddef2594eaa1ee28947
atlasrw <atlasrw@icloud.com>
2021-04-25 02:12:43 +0200

    clean

--- ruby_basics/exo_07_a.rb
+++
@@ -1,9 +0,0 @@
-puts "Bonjour, c'est quoi ton blase ?"
-
-user_name = gets.chomp
-#ça donne un formulaire libre pour entrer du texte
-
-puts user_name
-#puis ça l'affiche
-
-#c'est moyennement clair, on a la question mais on sait pas si on doit écrire
