51cde580329956c1f6e234283beded0f9dc5e472
atlasrw <atlasrw@icloud.com>
2021-04-25 02:00:36 +0200

    clean

--- ruby_basics/exo_09.rb
+++
@@ -1,8 +0,0 @@
-puts "Veuillez renseigner votre prénom :"
-print "> "
-prenom = gets.chomp
-puts "Veuillez renseigner votre nom :"
-print "> "
-nom = gets.chomp
-
-puts "Bonjour, #{prenom} #{nom} !"
--- ruby_basics/exo_18.rb
+++
@@ -1,6 +0,0 @@
-emails = []
-i = 0
-
-50.times do
-  emails << "email#{i = i + 1}@email.com"
-end
